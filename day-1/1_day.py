0a410bbea5231d96d469471a8919684418932387
Tymi <tymidzik@gmail.com>
2023-06-08 21:44:57 +0200

    Initial commit

---
+++ day-1/1_day.py
@@ -0,0 +1,7 @@
+print('Welcome to the Band Name Generator.')
+
+city = input("What's name of the city you grew up in?\n")
+
+pet = input("What's your pet's name?\n")
+
+band_name = print("Your brand name could be " + pet + " " + city)
---
+++ day-3/main.py
@@ -0,0 +1,17 @@
+weight = float(input("Enter your weight in kg:"))
+height = float(input("Enter your height in m:"))
+
+BMI = weight / height ** 2
+
+print(f"{weight} / ({height} * {height}) = {BMI}")
+
+if BMI < 18.5:
+    print(f"Your BMI is {BMI} you are underweight.")
+elif BMI >= 18.5 and BMI < 25:
+    print(f"Your BMI is {BMI} you are normal weight.")
+elif BMI >= 25 and BMI < 30:
+    print(f"Your BMI is {BMI} you are slightly overweight.")
+elif BMI >= 30 and BMI < 35:
+    print(f"Your BMI is {BMI} you are overweight")
+else:
+    print(f"Your BMI is {BMI} you are clinically overweight")
